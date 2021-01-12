25658bd81b71b21a272beb13df5def4ced609b38
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-13 00:03:43 +0530

    Angular Project - Dynamic Search Component - Call Product Service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,3 +1,4 @@
+import { ProductCategory } from './../common/product-category';
 import { Product } from './../common/product';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -9,7 +10,10 @@ import { map } from 'rxjs/operators';
 })
 export class ProductService {
 
+
   private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,14 +22,26 @@ export class ProductService {
     //need to build url based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
