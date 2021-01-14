f24a417e3bfebdc9c42c98eb4695f955c4bd80a1
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-15 00:12:06 +0530

    Product -Detail - Enhance the Details Component

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
+import { ProductService } from './../../services/product.service';
+import { Product } from './../../common/product';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product;
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductetails();
+    })
+  }
+  handleProductetails() {
+    //get the "id" param string 
+    //convert it into int
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
   }
 
 }
